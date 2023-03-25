207be865e3810424d7da6765f80b601425b2ceb2
trajchevaM <118018439+trajchevaM@users.noreply.github.com>
2023-03-25 15:19:01 +0100

    Init

---
+++ emt-sb/src/main/java/com/example/emtsb/EmtSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.emtsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmtSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmtSbApplication.class, args);
+	}
+
+}
---
+++ emt-sb/src/test/java/com/example/emtsb/EmtSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emtsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmtSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
