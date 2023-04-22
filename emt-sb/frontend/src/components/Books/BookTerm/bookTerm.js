63a635cbf254858677e7a015529d7af62f0c2aa0
trajchevaM <118018439+trajchevaM@users.noreply.github.com>
2023-04-22 12:50:18 +0200

    changes made to work

--- emt-sb/frontend/src/components/Books/BookTerm/bookTerm.js
+++ emt-sb/frontend/src/components/Books/BookTerm/bookTerm.js
@@ -3,14 +3,11 @@ import React from "react";
 const bookTerm = (props) => {
     return (
         <tr>
-            <th>
-                <tr>
                     <td scope={"col"}>{props.term.name}</td>
                     <td scope={"col"}>{props.term.category}</td>
-                    <td scope={"col"}>{props.term.author}</td>
+                    <td scope={"col"}>{props.term.author.name} {props.term.author.surname}</td>
                     <td scope={"col"}>{props.term.availableCopies}</td>
-                </tr>
-            </th>
+
         </tr>
     )
 }
